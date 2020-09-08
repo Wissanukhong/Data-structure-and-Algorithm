99e1bdd1dac7be6dd002f24e4f80d07793f1c6c0
Wissanu.khong <wissanu.khong@gmail.com>
2020-09-08 22:53:31 +0700

    refac: find duplicate number

--- 2_findDuplicateNumber.js
+++ 2_findDuplicateNumber.js
@@ -1,10 +1,15 @@
 function findDuplicateNumber() {
-    let arr = [1,2,3,3,4];
+    let arr = [1, 2, 3, 3, 4];
     let result = arr.filter(a => (arr.filter(b => (a == b)).length != 1));
-    if(result.length >= result){
-        console.log(result)
-    }else 
-    console.log("-1")
+
+    if (result.length == 0) {
+        result = -1;
+    }
+
+    console.log(result);
 }
 
 findDuplicateNumber()
+
+
+// https://i.learnalgorithm.com/lesson/2-time-complexity-analysis
