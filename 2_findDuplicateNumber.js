41c1b6bb19538a8751cfedfc0e554f512b0f7ca6
Wissanu.khong <wissanu.khong@gmail.com>
2020-09-08 22:27:53 +0700

    refac: find duplicate number

--- 2_findDuplicateNumber.js
+++ 2_findDuplicateNumber.js
@@ -1,8 +1,10 @@
 function findDuplicateNumber() {
     let arr = [1,2,3,3,4];
     let result = arr.filter(a => (arr.filter(b => (a == b)).length != 1));
-    return result
+    if(result.length >= result){
+        console.log(result)
+    }else 
+    console.log("-1")
 }
 
-findDuplicateNumber();
-console.log(result)
+findDuplicateNumber()
