94943b307f1e4f9a5a6a9470c2c3432d8844316c
Wissanu.khong <wissanu.khong@gmail.com>
2020-09-09 22:18:15 +0700

    debug : find duplicate Number

--- 1_FunctionAddNumber.js
+++ 1_FunctionAddNumber.js
@@ -1,17 +1,48 @@
-// ให้เขียน Function ชื่อว่า add โดยที่รับตัวแปรมา 1 ตัวเป็นตัวเลข (เป็นจำนวนเต็มบวกที่มีค่ามากกว่า 0) 
-// และให้ผลลัพธ์ออกมาเป็นผลบวกตั้งแต่เลข 1 ถึงตัวเลขนั้น เช่น Input = 3 จะได้ Output = 6 เพราะว่า 1 + 2 + 3 = 6
-function add(n) {
-    let result = 0;
-    for (let i = 1; i <= n; ++i) {
-        result += i;
+// solution 1
+const findDuplicateNumber = (arr) => {
+    let results = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i + 1] == arr[i]) {
+            results.push(arr[i]);
+        }
+
+        mm = results
+
     }
-    return result
-}
-console.log(add(3));
+    if ((mm.length < 1)) {
+        return -1;
+    }
+    return mm
+};
+
+let duplicatedArray = [1, 2, 3, 3, 4];
+console.log(findDuplicateNumber(duplicatedArray));
 
 // solution 2
-function add2(n) {
-    return (n + 1) * n / 2;
+function findDuplicateNumber(arr) {
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = i + 1; j < arr.length; ++j) {
+            if (arr[i] === arr[j]) return arr[i];
+        }
+    }
+    return -1;
 }
 
-console.log(add2(5));
+// solution 3
+function findDuplicateNumber(arr) {
+    arr.sort();
+    for (let i = 0; i < arr.length - 1; ++i) {
+        if (arr[i] === arr[i+1]) return arr[i];
+    }
+    return -1;
+}
+
+// solution 4 
+function findDuplicateNumber(arr) {
+    const checker = {};
+    for (let i = 0; i < arr.length; ++i) {
+        if (checker[arr[i]]) return arr[i];
+        checker[arr[i]] = true;
+    }
+    return -1;
+}
